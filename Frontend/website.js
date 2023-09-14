bcca53f624579589510b5365dc8304a6361c36b5
virendrapatil0750 <virus.6patil@gmail.com>
2023-09-14 16:41:18 +0530

    new changes to the newUser form. when clicked upon the toggle it would generate the form .

--- Frontend/website.js
+++ Frontend/website.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    $(selector).toggleClass(className, switch);
+    $('#sliderToggle').change(function(){
+        if(this.checked){
+            $('#registerationForm').removeClass('hidden');
+            console.log("Working properly");
+        }
+        else{
+            $('#registerationForm').addClass('hidden');
+        }
+    })
 });
